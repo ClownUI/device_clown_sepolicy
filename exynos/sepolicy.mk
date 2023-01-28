bb186bcaed31d4caad9c5e5c53ecc32a80b3ba0b
NurKeinNeid <mralexman3000@gmail.com>
2023-01-28 21:14:24 +0100

    sepolicy: Adjust sepolicy paths for ClownUI
    Change-Id: Icc63869779e98ef592a7f1d50a5ddbffeb4aa6ab
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- exynos/sepolicy.mk
+++ exynos/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by all exynos products
-# that inherit from Lineage
+# that inherit from ClownUI
 #
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
@@ -10,14 +10,14 @@ endif
 endif
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/exynos/private
+    device/clown/sepolicy/exynos/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/exynos/dynamic \
-    device/lineage/sepolicy/exynos/system
+    device/clown/sepolicy/exynos/dynamic \
+    device/clown/sepolicy/exynos/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/exynos/dynamic \
-    device/lineage/sepolicy/exynos/vendor
+    device/clown/sepolicy/exynos/dynamic \
+    device/clown/sepolicy/exynos/vendor
 endif
