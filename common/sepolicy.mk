60d15dc2ba6bf58636486b08a74f8f79b3314e0c
strongreasons <abenkenari@gmail.com>
2024-08-25 09:01:42 +0000

    sepolicy: Adapt path for Clown
    Change-Id: I8fb4f61e98ba65c2d682595813b1de04d7d0f2f2
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from Clown
 #
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
@@ -10,26 +10,26 @@ endif
 endif
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/public
+    device/clown/sepolicy/common/public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/private
+    device/clown/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/system \
+    device/clown/sepolicy/common/dynamic \
+    device/clown/sepolicy/common/system \
     hardware/google/pixel-sepolicy/flipendo \
     hardware/google/pixel-sepolicy/turbo_adapter
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/vendor \
+    device/clown/sepolicy/common/dynamic \
+    device/clown/sepolicy/common/vendor \
     hardware/google/pixel-sepolicy/flipendo \
     hardware/google/pixel-sepolicy/turbo_adapter
 endif
 
 # Include atv rules on atv product
 ifeq ($(PRODUCT_IS_ATV), true)
-include device/lineage/sepolicy/atv/sepolicy.mk
+include device/clown/sepolicy/atv/sepolicy.mk
 endif
